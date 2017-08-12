fe0a5d8070e1c9e69baa89ca232aafee66209cca
jeff3456 <jeff3456@gmail.com>
2017-08-12 14:05:57 -0400

    added a bunch of tutorial games

---
+++ StarExplorer/main.lua
@@ -0,0 +1,22 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+local composer = require( "composer" )
+
+-- Hide status bar
+display.setStatusBar( display.HiddenStatusBar )
+
+-- Seed the random number generator
+math.randomseed( os.time() )
+
+-- Go to the menu screen
+composer.gotoScene( "menu" )
+
+-- Reserve channel 1 for background music
+audio.reserveChannels( 1 )
+-- Reduce the overall volume of the channel
+audio.setVolume( 0.5, { channel=1 } )
